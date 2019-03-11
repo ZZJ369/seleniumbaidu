6714f6bd24a4a09abcd0c26785dbcec79a3cc3a7
ZZJ369 <2298644127@qq.com>
2019-03-11 14:16:36 +0800

    3月11日下午提交

---
+++ test/src/test/Sizeyunsuan.java
@@ -0,0 +1,23 @@
+package test;
+
+public class Sizeyunsuan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a=1;
+		double b=2;
+		System.out.println("a-b=" +(a-b));
+		System.out.println("a+b=" +(a+b));
+		System.out.println("a*b=" +(a*b));
+		System.out.println("a/b=" +(a/b));
+		if(a!=1) {
+			System.out.println("a-b=" +(a-b));
+			
+			}else{
+				System.out.println("a/b=" +(a/b));
+		
+		}
+		
+	}
+
+}
